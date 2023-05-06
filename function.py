b453d0a7f68ec63cff9d358cc0027eb1570605b3
Sidhant_sarangi2 <sidhantsarangi2@gmail.com>
2023-05-05 23:53:21 -0700

    added a "add feature"

--- function.py
+++ function.py
@@ -1,11 +1,14 @@
-def readfile(parameter='mem.txt'):
+FILEPATH = "mem.txt"
+
+
+def readfile(parameter=FILEPATH):
     """ reads an already existing file and readlines command assigns a list to a variable list local"""
     with open(parameter, 'r') as file_local:
         list_local = file_local.readlines()
         return list_local
 
 
-def writefile(list_arg,parameter='mem.txt'):
+def writefile(list_arg, parameter=FILEPATH):
     """uses a text file to edit info"""
-    with open(parameter,'w') as file_local2:
+    with open(parameter, 'w') as file_local2:
         file_local2.writelines(list_arg)
--- gui_version.py
+++ gui_version.py
@@ -1,8 +1,25 @@
-import function
+import PySimpleGUI
+
+from function import readfile, writefile
 import PySimpleGUI as ps
 label = ps.Text("type in a todo")
-input_ = ps.InputText(tooltip="what do you want to do")
-add_button = ps.Button('Add')
-window = ps.Window('To-do app', layout=[[label], [input_, add_button]])
-window.read()
+input_ = ps.InputText(tooltip="what do you want to do", key="todo")
+add_button = ps.Button('add')
+listbox = ps.Listbox(values=readfile(), key="todos",enable_events=True,size=[45, 10])
+window = ps.Window('To-do app',
+                   layout=[[label], [input_, add_button]],
+                   font=("Helvetica", 16))
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "add":
+            list1 = readfile()
+            new_todo = value["todo"] + '\n'
+            list1.append(new_todo)
+            writefile(list1)
+        case ps.WIN_CLOSED:
+            break
+
 window.close()
